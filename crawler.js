a26bcd84708ae25746cabb856432c642e9cf06b5
Pat V <vuong.patrick@outlook.com>
2021-05-28 19:24:42 +1000

    Initial commit

---
+++ crawler.js
@@ -0,0 +1,96 @@
+const request = require('request')
+const cheerio = require('cheerio')
+const URL = require('url-parse')
+const e = require('express')
+
+const SEARCH = 'Pricing Error'
+const BASE = 'https://www.ozbargain.com.au/'
+const START_URL = 'https://www.ozbargain.com.au/deals'
+const url = new URL(START_URL)
+var pagesToVisit = []
+var pagesVisited = []
+
+const crawler = () => {
+  //Initial Link collection
+  request(START_URL, (error, res, body) => {
+    console.log(`Visiting ${START_URL} to gather links...`)
+    if (error) {
+      console.log(error)
+    }
+
+    //Check status code 200
+    console.log(`Initial page status code: ${res.statusCode}`)
+    if (res.statusCode !== 200) {
+      console.log('Failed inital page load')
+      return
+    } else {
+      var $ = cheerio.load(body)
+
+      var relativeLinks = $("a[href^='/node']")
+      relativeLinks.each(function () {
+        pagesToVisit.push(url.origin + $(this).attr('href'))
+      })
+
+      console.log('Found ' + pagesToVisit.length + ' pages to visit')
+
+      pagesToVisit.push(START_URL)
+      crawl()
+    }
+  })
+}
+
+const crawl = () => {
+  console.log(pagesVisited.length)
+  var nextPage = pagesToVisit.pop()
+  if (pagesToVisit.length === 0) {
+    console.log('Finished crawling')
+  } else {
+    setTimeout(() => {
+      visitPage(nextPage, crawl)
+    }, 3000)
+  }
+}
+
+const visitPage = (url, callback) => {
+  // Add page to our set
+  pagesVisited.push(url)
+
+  request(url, (error, res, body) => {
+    console.log(`Visiting page ${url}`)
+    if (error) {
+      console.log(error)
+    }
+
+    //Check status code 200
+    console.log(`Status Code: ${res.statusCode}`)
+    if (res.statusCode !== 200) {
+      //Initial site must return status code 200, otherwise pagesToVisit remains empty
+      callback()
+      return
+    }
+
+    if (res.statusCode === 200) {
+      //Parse document body
+      var $ = cheerio.load(body)
+      var isWordFound = searchForWord($, SEARCH)
+      if (isWordFound) {
+        console.log('Word "' + SEARCH + '" found at page ' + url)
+        callback()
+      } else {
+        console.log('Word ' + '"' + SEARCH + '"' + ' NOT found at page ' + url)
+        callback()
+      }
+    }
+  })
+}
+
+//Search for word
+const searchForWord = ($, word) => {
+  var bodyText = $('html > body').text()
+  if (bodyText.toLowerCase().indexOf(word.toLowerCase()) !== -1) {
+    return true
+  }
+  return false
+}
+
+crawler()
